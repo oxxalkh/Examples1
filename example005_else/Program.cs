884482a9adf625aec7f64f132c631783a27b59ff
oxanaalkh <ksenikrsk@mail.ru>
2022-11-29 18:16:50 +0300

    add example005

---
+++ example005_else/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("введите имя пользователя");
+string username = Console.Readline();
+
+if (username == "Маша");
+
+{
+    Console.WriteLine("Ура");
+}
+
+else
+{
+   Console.Write("Ну привет");
+   Console.WriteLine(username); 
+}
+
+
+
+
