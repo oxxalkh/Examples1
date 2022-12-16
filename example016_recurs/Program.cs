e4523e895606cc18e8fbdb01f22d5dddc883423b
oxanaalkh <ksenikrsk@mail.ru>
2022-12-16 14:39:13 +0300

    lection 4

---
+++ example016_recurs/Program.cs
@@ -0,0 +1,22 @@
+﻿//факториал
+
+// int Factorial(int n)
+// {
+//  // 1! = 1
+//  // 0! = 1
+//  if(n == 1) return 1;
+//  else return n * Factorial(n-1);
+// }
+// Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
+
+//числа фиброначчи
+
+int Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+ Console.WriteLine(Fibonacci(i));
+}
